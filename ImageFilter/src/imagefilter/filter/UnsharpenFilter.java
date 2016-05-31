af4b7d7dc10e0f6fe00eb8521bc520de1ef17c0e
hoellinger <hoellinger@hoellinger-lapi>
2016-05-31 14:53:12 +0200

    dynamisches laden der previews

--- ImageFilter/src/imagefilter/filter/UnsharpenFilter.java
+++ ImageFilter/src/imagefilter/filter/UnsharpenFilter.java
@@ -26,7 +26,7 @@ public class UnsharpenFilter implements FilterInterface {
 
     @Override
     public ImageIcon getPreview() {
-        return new ImageIcon(Tools.getResource("SampleImages/UnsharpenFilter.jpg"));
+        return preview;
     }
 
     @Override
